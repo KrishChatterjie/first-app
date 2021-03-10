0bd4fca89baae4eb181a84e35fda8d1d2c69dd72
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-03-10 21:47:38 +0530

    feat: created simple number game

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Button, Pressable} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Header from './components/Header';
 
 const App = () => {
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
   const [count, setCount] = useState(0);
+  const [time, setTime] = useState(15);
+  const [random, setRandom] = useState(Math.floor(Math.random() * 10));
+
+  const STORAGE_KEY = '@save_highscore';
+
+  const readData = async () => {
+    try {
+      let high = await AsyncStorage.getItem(STORAGE_KEY);
+      high = parseInt(high, 10);
+      if (high !== null) {
+        setHighscore(high);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const reset = () => {
+    setScore(0);
+    setCount(0);
+    setTime(15);
+    setRandom(Math.floor(Math.random() * 10));
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(time - 1);
+      if (time === 0) {
+        setTime(0);
+      }
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, [time, score, highscore]);
+
+  useEffect(() => {
+    if (time === 0 || time === 15) {
+      if (score > highscore) {
+        setHighscore(score);
+        const saveData = async () => {
+          try {
+            await AsyncStorage.setItem(STORAGE_KEY, score.toString());
+          } catch (e) {
+            console.log(e);
+          }
+        };
+        saveData();
+      }
+      readData();
+    }
+  }, [setHighscore, highscore, score, time]);
+
+  useEffect(() => {
+    if (count === random) {
+      setRandom(Math.floor(Math.random() * 10));
+      setScore(score + 1);
+    }
+  }, [count, random, score]);
+
   return (
     <View style={styles.container}>
-      <Header />
-      <View style={styles.div}>
-        <Button
-          title="Click Me!"
-          onPress={() => setCount(count + 1)}
-          color="#152238"
-        />
-        <Text style={styles.text}>{count} clicks!</Text>
+      <Header highscore={highscore} time={time} />
+      <Text style={styles.text}>Need to reach: {random}</Text>
+      <Text style={styles.text}>Current: {count}</Text>
+      <View style={styles.buttonDiv}>
+        <Pressable
+          disabled={time === 0}
+          style={() => [
+            styles.button,
+            {
+              backgroundColor: time !== 0 ? '#05b041' : 'lightgrey',
+            },
+          ]}
+          onPress={() => setCount(count + 1)}>
+          <Text style={styles.buttonText}>+</Text>
+        </Pressable>
+        <Pressable
+          disabled={time === 0}
+          style={() => [
+            styles.button,
+            {
+              backgroundColor: time !== 0 ? '#ad0e05' : 'lightgrey',
+            },
+          ]}
+          onPress={() => setCount(count - 1)}>
+          <Text style={styles.buttonText}>-</Text>
+        </Pressable>
       </View>
+      <Text style={styles.buttonText}>Score: {score}</Text>
+      <Button title="Reset" onPress={reset} />
     </View>
   );
 };
@@ -24,16 +105,32 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: 'bold',
-    color: '#23395d',
+    color: '#0e0e0e',
     margin: 20,
+    justifyContent: 'flex-start',
     alignSelf: 'center',
   },
-  div: {
+  buttonDiv: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 0.35,
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: 'lightgrey',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 30,
+    margin: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
   },
 });
 
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const App = () => {
+const App = ({highscore, time}) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.text}>Header</Text>
+      <Text style={styles.text}>Highscore: {highscore}</Text>
+      <Text style={styles.text}>Time: {time}</Text>
     </View>
   );
 };
@@ -13,7 +14,9 @@ const styles = StyleSheet.create({
   header: {
     height: 60,
     padding: 15,
-    backgroundColor: '#152238',
+    backgroundColor: '#0e0e0e',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   text: {
     color: '#fff',
